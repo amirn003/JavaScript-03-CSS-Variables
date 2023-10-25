74a64eb74d35b80a3c10f95aa632d7be0c6c1f72
amirn003 <amir.naar@gmail.com>
2023-10-25 23:45:45 +0200

    change image property dynamically

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const inputs = document.querySelectorAll('.controls input');
 console.log(inputs);
 
 function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
   console.log(this.value);
+  console.log(suffix);
 }
 
 inputs.forEach((input) => {
   input.addEventListener('change', handleUpdate);
+})
+
+
+inputs.forEach((input) => {
+  input.addEventListener('mousemove', handleUpdate);
 })
